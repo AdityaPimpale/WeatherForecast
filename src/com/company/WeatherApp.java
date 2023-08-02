1d1f82bfabe2c02a8f972df852a8579611220075
Aditya Pimpale <95747968+AdityaPimpale@users.noreply.github.com>
2023-08-02 10:16:14 +0530

    Update WeatherApp.java

--- src/com/company/WeatherApp.java
+++ src/com/company/WeatherApp.java
@@ -76,7 +76,7 @@ public class WeatherApp {
     }
 
     private String fetchAPIResponse(String date) {
-        String apiUrl = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=" + API_KEY;
+        String apiUrl = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22" + API_KEY;
 
         try {
             URL url = new URL(apiUrl);
